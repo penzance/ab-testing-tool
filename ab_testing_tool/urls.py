c8f96206b1c7d4dc6e560b4033eb787c880e94a3
Benjamin Zagorsky <iamzags@gmail.com>
2014-08-04 14:56:00 -0400

    adds autogenerating xml config to tool under page tool_config includes basic resource selection config for the tool

--- ab_testing_tool/urls.py
+++ ab_testing_tool/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from ab_testing_tool.views import index, not_authorized
+from ab_testing_tool.views import index, not_authorized, tool_config, lti_launch
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', index),
-    url(r'^not_authorized$', not_authorized, name='not_authorized')
+    url(r'^not_authorized$', not_authorized, name='not_authorized'),
+    url(r'^lti_launch$', lti_launch, name="lti_launch"),
+    url(r'^tool_config$', tool_config, name='tool_config'),
+    
     # Examples:
     # url(r'^$', 'project_name.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
--- ab_testing_tool/views.py
+++ ab_testing_tool/views.py
@@ -1,18 +1,61 @@
 from django.http.response import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.clickjacking import xframe_options_exempt
-from django.core.urlresolvers import reverse_lazy
+from django.core.urlresolvers import reverse
 from django.shortcuts import render, render_to_response, redirect
 
 from django_auth_lti.decorators import lti_role_required
 from django_auth_lti.const import (ADMINISTRATOR, CONTENT_DEVELOPER,
     TEACHING_ASSISTANT, INSTRUCTOR)
+from ims_lti_py.tool_config import ToolConfig
+from django.views.decorators.http import require_http_methods
 
 ADMINS = [ADMINISTRATOR, CONTENT_DEVELOPER, TEACHING_ASSISTANT, INSTRUCTOR]
 
 @lti_role_required(ADMINS)
 def index(request):
-    return render_to_response("admin.html")
+    #print request
+    #print request.__dict__
+    return HttpResponse("index")
+    #return render_to_response("admin.html")
 
 def not_authorized(request):
     return HttpResponse("Student")
+
+def lti_launch(request):
+    #return redirect("/")
+    return HttpResponse("lti_launch")
+
+@require_http_methods(["GET"])
+def tool_config(request):
+    if request.is_secure():
+        host = "https://" + request.get_host()
+    else:
+        host = "http://" + request.get_host()
+    
+    url = host + reverse("lti_launch")
+    
+    config = ToolConfig(
+        title="A/B Testing Tool",
+        launch_url=url,
+        secure_launch_url=url,
+    )
+    # Tell Canvas that this tool provides a course navigation link:
+    nav_params = {
+        "enabled": "true",
+        # optionally, supply a different URL for the link:
+        # "url": "http://library.harvard.edu",
+        "text": "A/B Testing Tool",
+        "visibility": "admins",
+    }
+    config.set_ext_param("canvas.instructure.com", "privacy_level", "public")
+    config.set_ext_param("canvas.instructure.com", "course_navigation",
+                         nav_params)
+    config.set_ext_param("canvas.instructure.com", "resource_selection",
+                         {"enabled": "true", "url": host + reverse("index")})
+    config.set_ext_param("canvas.instructure.com", "selection_height", "400")
+    config.set_ext_param("canvas.instructure.com", "selection_width", "600")
+    config.set_ext_param("canvas.instructure.com", "tool_id", "ab_testing_tool")
+    config.description = ("Tool to allow students in a course to " +
+                          "get different conent in a module item.")
+    
+    resp = HttpResponse(config.to_xml(), content_type="text/xml", status=200)
+    return resp
