00f1c636348133f627a90d9008c495ec8054c113
kfantastic <kyangfan@gmail.com>
2014-08-06 18:16:30 -0400

    treatment control panel, modules sdk integration, treatment model, etc.

---
+++ ab_testing_tool/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Treatment(models.Model):
+    name = models.CharField(max_length=512)
+    notes = models.CharField(max_length=1000)
+    course_id = models.CharField(max_length=12, db_index=True)
+    treatment_url1 = models.CharField(max_length=512)
+    treatment_url2 = models.CharField(max_length=512)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
--- ab_testing_tool/urls.py
+++ ab_testing_tool/urls.py
@@ -1,15 +1,23 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from ab_testing_tool.views import index, not_authorized
+from ab_testing_tool.views import render_treatment_control_panel, not_authorized, deploy_treatment, update_treatment, get_treatment
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', index),
-    url(r'^not_authorized$', not_authorized, name='not_authorized')
+    url(r'^$', render_treatment_control_panel, name='index'),
+    url(r'^not_authorized$', not_authorized, name='not_authorized'),
+    url(r'^update_treatment$', update_treatment, name='update_treatment'),
+
+    url(r'^edit_treatment/$', get_treatment, name='edit_treatment'),
+    url(r'^edit_treatment/(?P<t_id>\d+)$', get_treatment, name='edit_treatment'),
+
+
+    url(r'^treatment/(?P<t_id>\d+)$', deploy_treatment, name='render_treatment'),
+
+    url(r'^(?P<unique_id>\d+)/(?P<t_id>\d+)$', deploy_treatment, name='render_treatment'),
     # Examples:
     # url(r'^$', 'project_name.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     # url(r'^admin/', include(admin.site.urls)),
 )
