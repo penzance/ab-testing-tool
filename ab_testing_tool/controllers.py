fc675a726ccdb487aaf5d1c56fd15d8e2371d9af
Benjamin Zagorsky <iamzags@gmail.com>
2014-08-07 20:22:48 -0400

    cleans up older code, in particular eliminating lines over 80 chars and moving several functions out of views changes unused_treatments to check by url rather than name for usage integrates treatments logic with the resource selection dialogue

--- ab_testing_tool/controllers.py
+++ ab_testing_tool/controllers.py
@@ -1,25 +1,60 @@
-from ab_testing_tool.settings.secure import COURSE_OAUTH_TOKEN #Needed token to interact with Canvas, #CHANGEME to developer token
-from canvas_sdk import RequestContext
 import json
+from canvas_sdk import RequestContext
+from canvas_sdk.methods.modules import list_module_items, list_modules
 
+from ab_testing_tool.models import Treatment
+#TODO: change from secure.py setting to oauth handoff
+from ab_testing_tool.settings.secure import COURSE_OAUTH_TOKEN
 
-def _get_lti_param(request, key):
-    """TODO: Add this function to Django auth lti library"""
-    return request.session["LTI_LAUNCH"][key]
-
-def _establish_canvas_sdk(request):
-    oauth_token = COURSE_OAUTH_TOKEN
-    canvas_url = "https://{0}/api".format(_get_lti_param(request, "custom_canvas_api_domain"))
-    return RequestContext(oauth_token, canvas_url)
 
 class InvalidResponseError(Exception):
-    """TODO: Move this to canvas_sdk_python"""
+    """ TODO: Move this to canvas_sdk_python """
     pass
 
+
+def get_lti_param(request, key):
+    """ TODO: Add this function to Django auth lti library """
+    return request.session["LTI_LAUNCH"][key]
+
+
+def get_canvas_request_context(request):
+    oauth_token = COURSE_OAUTH_TOKEN
+    canvas_url = "https://{0}/api".format(get_lti_param(request, "custom_canvas_api_domain"))
+    return RequestContext(oauth_token, canvas_url)
+
+
 def parse_response(sdk_response):
-    """TODO: Move this function to canvas_sdk_python so that all sdk functions return python objects instead of response objects"""
+    """ TODO: Move this function to canvas_sdk_python so that all sdk
+        functions return python objects instead of response objects """
     if sdk_response.ok:
         return json.loads(sdk_response.text)
     else:
         raise InvalidResponseError
 
+
+def get_full_host(request):
+    if request.is_secure():
+        return "https://" + request.get_host()
+    else:
+        return "http://" + request.get_host()
+
+
+def treatment_url(request, t_id):
+    return "%s/treatment/%s" % (get_full_host(request), t_id)
+
+
+def get_uninstalled_treatments(request):
+    course_id = get_lti_param(request, "custom_canvas_course_id")
+    request_context =  get_canvas_request_context(request)
+    response = list_modules(request_context, course_id, "content_details")
+    all_modules = parse_response(response)
+    installed_treatment_urls = []
+    for module in all_modules:
+        response = list_module_items(request_context, course_id,
+                                     module["id"], "content_details")
+        treatment_urls = [t["external_url"] for t in parse_response(response)
+                          if t["type"] == "ExternalTool"]
+        installed_treatment_urls.extend(treatment_urls)
+    treatments = [t for t in Treatment.objects.all()
+                  if treatment_url(request, t.id) not in installed_treatment_urls]
+    return treatments
--- ab_testing_tool/views.py
+++ ab_testing_tool/views.py
@@ -1,56 +1,49 @@
+from canvas_sdk.methods.modules import (list_module_items, list_modules,
+    create_module_item)
+from django.core.urlresolvers import reverse
+from django.http.response import HttpResponse
+from django.shortcuts import render_to_response, redirect
 from django.utils.http import urlencode
 from django.views.decorators.http import require_http_methods
-from django.core.urlresolvers import reverse
-from django.shortcuts import render_to_response, redirect
-from django.http.response import HttpResponse
 #from django.views.decorators.csrf import csrf_exempt
 #from django.views.decorators.clickjacking import xframe_options_exempt
 from django_auth_lti.decorators import lti_role_required
 from django_auth_lti.const import (ADMINISTRATOR, CONTENT_DEVELOPER,
     TEACHING_ASSISTANT, INSTRUCTOR)
-from canvas_sdk.methods import modules
 from ims_lti_py.tool_config import ToolConfig
 from random import getrandbits, randint
 
+from ab_testing_tool.controllers import (get_lti_param, get_canvas_request_context,
+    parse_response, get_uninstalled_treatments, treatment_url, get_full_host)
 from ab_testing_tool.models import Treatment
-from ab_testing_tool.controllers import _get_lti_param, _establish_canvas_sdk, parse_response
 
 
 ADMINS = [ADMINISTRATOR, CONTENT_DEVELOPER, TEACHING_ASSISTANT, INSTRUCTOR]
 
-@lti_role_required(ADMINS)
-def index(request):
-    #print request
-    #print request.__dict__
-    return HttpResponse("index")
-    #return render_to_response("admin.html")
 
 def not_authorized(request):
     return HttpResponse("Student")
 
+
 @lti_role_required(ADMINS)
 def render_treatment_control_panel(request):
-    """
-    Renders control panel.
-    TODO: filter installed treatments by external_url instead of by treatment name
-    """
-    course_id = _get_lti_param(request, "custom_canvas_course_id")
-    request_context =  _establish_canvas_sdk(request)
-    all_modules = parse_response(modules.list_modules(request_context, course_id, "content_details"))
-    installed_treatments = []
-    for module in all_modules:
-        module["module_items"] = parse_response(modules.list_module_items(request_context, course_id, module["id"], "content_details"))
-        installed_treatments.extend([t["title"] for t in module["module_items"]])
-    treatments = [t for t in Treatment.objects.all() if t.name not in installed_treatments]
-    return render_to_response("control_panel.html", {"modules": all_modules,
-                                             "treatments": treatments,
-                                             "canvas_url": _get_lti_param(request, "launch_presentation_return_url")
-                                             })
+    course_id = get_lti_param(request, "custom_canvas_course_id")
+    request_context = get_canvas_request_context(request)
+    response = list_modules(request_context, course_id, "content_details")
+    all_modules = parse_response(response)
+    treatments = get_uninstalled_treatments(request)
+    
+    context = {"modules": all_modules,
+               "treatments": treatments,
+               "canvas_url": get_lti_param(request, "launch_presentation_return_url")
+              }
+    return render_to_response("control_panel.html", context)
+
 
 def deploy_treatment(request, t_id):
     """
-    Delivers randomly one of the two urls in treatment. TODO: Extend this by delivering
-    treatment as determined by track student is on"
+    Delivers randomly one of the two urls in treatment.
+    TODO: Extend this by delivering treatment as determined by track student is on
     """
     t = Treatment.objects.get(pk=t_id)
     if bool(getrandbits(1)):
@@ -58,13 +51,16 @@ def deploy_treatment(request, t_id):
     else:
         return redirect(t.treatment_url2)
 
+
 @lti_role_required(ADMINS)
 def new_treatment(request):
     """
-    Note: Canvas fetches all pages within iframe with POST request, requiring separate template render function.
-    This also breaks CSRF token validation if CSRF Middleware is turned out.
+    Note: Canvas fetches all pages within iframe with POST request,
+    requiring separate template render function.
+    This also breaks CSRF token validation if CSRF Middleware is turned off.
     """
     return render_to_response("edit_treatment.html")
+
 
 @lti_role_required(ADMINS)
 def create_treatment(request):
@@ -72,7 +68,7 @@ def create_treatment(request):
     Note: request will always be POST because Canvas fetches pages within iframe by POST
     TODO: use Django forms library to save instead of getting individual POST params
     """
-    course_id = _get_lti_param(request, "custom_canvas_course_id")
+    course_id = get_lti_param(request, "custom_canvas_course_id")
     name = request.POST["name"]
     url1 = request.POST["url1"]
     url2 = request.POST["url2"]
@@ -80,13 +76,14 @@ def create_treatment(request):
     Treatment.objects.create(name=name, treatment_url1=url1, treatment_url2=url2, notes=notes, course_id=course_id)
     return redirect("/")
 
+
 @lti_role_required(ADMINS)
 def update_treatment(request):
     """
     Update treatment only allowed if admin has privileges on the particular course.
     TODO: use Django forms library to save instead of getting individual POST params
     """
-    course_id = _get_lti_param(request, "custom_canvas_course_id")
+    course_id = get_lti_param(request, "custom_canvas_course_id")
     name = request.POST["name"]
     url1 = request.POST["url1"]
     url2 = request.POST["url2"]
@@ -101,27 +98,40 @@ def update_treatment(request):
         raise Exception("No treatment found")
     return redirect("/")
 
+
 @lti_role_required(ADMINS)
 def edit_treatment(request, t_id):
-    return render_to_response("edit_treatment.html", {"treatment": Treatment.objects.get(pk=t_id)})
+    return render_to_response("edit_treatment.html",
+                              {"treatment": Treatment.objects.get(pk=t_id)})
+
 
 @lti_role_required(ADMINS)
 def add_treatment_to_module(request, t_id):
     """
-    TODO: Finish this to be able to add treatment to a module from A/B/ control panel
+    TODO: Finish this to be able to add treatment to a module from control panel
     """
-    course_id = _get_lti_param(request, "custom_canvas_course_id")
-    request_context =  _establish_canvas_sdk(request)
-    modules = modules.list_modules(request_context, course_id, "content_details")
-    item_results = modules.list_module_items(request_context, course_id, "24", "content_details")
-    #DOC: canvas_sdk.methods.modules.create_module_item(request_ctx, course_id, module_id, module_item_type, module_item_content_id, module_item_page_url, module_item_external_url, module_item_completion_requirement_min_score
-    modules.create_module_item(request_context, course_id, "27", "ExternalTool", "ab_testing_tool", "1", "http://localhost:8000/treatment/{0}".format(t_id), None)
+    course_id = get_lti_param(request, "custom_canvas_course_id")
+    request_context =  get_canvas_request_context(request)
+    modules = list_modules(request_context, course_id, "content_details")
+    item_results = list_module_items(request_context, course_id,
+                                     "24", "content_details")
+    module_id = modules[0]["id"]
+    # canvas_sdk.methods.modules.create_module_item(
+    #        request_ctx, course_id, module_id, module_item_type,
+    #        module_item_content_id, module_item_page_url,
+    #        module_item_external_url,
+    #        module_item_completion_requirement_min_score)
+    create_module_item(request_context, course_id, module_id, "ExternalTool",
+                       "ab_testing_tool", "1", treatment_url(request, t_id),
+                       None)
     return redirect("/")
 
-def lti_launch(request):
-    return resource_selection(request)
 
-def resource_selection(request):
+def lti_launch(request):
+    return treatment_selection(request)
+
+
+def treatment_selection(request):
     """ docs: https://canvas.instructure.com/doc/api/file.link_selection_tools.html """
     ext_content_return_types = request.REQUEST.get('ext_content_return_types')
     if ext_content_return_types == [u'lti_launch_url']:
@@ -131,31 +141,26 @@ def resource_selection(request):
     if not content_return_url:
         return HttpResponse("Error: no ext_content_return_url")
     
-    return render_to_response("add_module_item.html",
-                              {"content_return_url": content_return_url})
+    context = {"content_return_url": content_return_url,
+               "treatments": get_uninstalled_treatments(request)}
+    return render_to_response("add_module_item.html", context)
+
 
 def submit_selection(request):
-    page_url = "/%s" % randint(1000000000, 9999999999) #TODO: improve uniqueness
-    page_name = request.REQUEST.get("page_name")#"A/B Page"
+    treatment_id = request.REQUEST.get("treatment_id")
+    page_url = treatment_url(request, treatment_id)
+    print treatment_id, page_url
+    page_name = "A/B Page" # TODO: replace with value from DB
     content_return_url = request.REQUEST.get("content_return_url")
-    
-    if request.is_secure():
-        host = "https://" + request.get_host()
-    else:
-        host = "http://" + request.get_host()
-    lti_launch_url = "%s%s" % (host, page_url)
     params = {"return_type": "lti_launch_url",
-               "url": lti_launch_url,
+               "url": page_url,
                #"title": "Title",
                "text": page_name}
     return redirect("%s?%s" % (content_return_url, urlencode(params)))
 
+
 def tool_config(request):
-    if request.is_secure():
-        host = "https://" + request.get_host()
-    else:
-        host = "http://" + request.get_host()
-    
+    host = get_full_host(request)
     url = host + reverse("lti_launch")
     
     config = ToolConfig(
