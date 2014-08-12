9e910cdbdc17b81692759bf141abb05e0880844d
Benjamin Zagorsky <iamzags@gmail.com>
2014-08-11 22:21:48 -0400

    adds tests for the get_uninstalled_treatmets function moves common logic into base class and constants creates APIReturn mock object to simplify test config

--- tests.py
+++ tests.py
@@ -3,9 +3,16 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils.importlib import import_module
 from json import dumps
-from mock import patch
+from mock import patch, MagicMock
 
 from ab_testing_tool.views import ADMINS
+from ab_testing_tool.controllers import get_uninstalled_treatments,\
+    treatment_url
+from ab_testing_tool.models import Treatment
+
+VIEWS_LIST_MODULES = "ab_testing_tool.views.list_modules"
+CONTROLLERS_LIST_MODULES = "ab_testing_tool.controllers.list_modules"
+CONTROLLERS_LIST_ITEMS = "ab_testing_tool.controllers.list_module_items"
 
 
 class SessionTestCase(TestCase):
@@ -27,28 +34,57 @@ class SessionTestCase(TestCase):
         session = self.client.session
         session['LTI_LAUNCH'] = lti_launch
         session.save()
+        
+        self.request = MagicMock()
+        self.request.session = session
+        self.request.get_host = MagicMock(return_value="example.com")
+    
+    def set_roles(self, roles):
+        session = self.client.session
+        session['LTI_LAUNCH']["roles"] = roles
+        session.save()
+
+
+class APIReturn(object):
+    def __init__(self, obj, ok=True):
+        self.text = dumps(obj)
+        self.ok = ok
 
 
 class test_views(SessionTestCase):
-    
-    @patch("ab_testing_tool.views.list_modules")
-    @patch("ab_testing_tool.controllers.list_modules")
-    def test_index(self, list_modules_mock1, list_modules_mock2):
-        list_modules_mock1.return_value.text = dumps([])
-        list_modules_mock2.return_value.text = dumps([])
+    @patch(VIEWS_LIST_MODULES, return_value=APIReturn([]))
+    @patch(CONTROLLERS_LIST_MODULES, return_value=APIReturn([]))
+    def test_index(self, _mock1, _mock2):
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "control_panel.html")
     
-    @patch("ab_testing_tool.controllers.list_module_items")
-    @patch("ab_testing_tool.views.list_modules")
-    @patch("ab_testing_tool.controllers.list_modules")
-    def test_index_with_module(self, list_modules_mock1, list_modules_mock2,
-                               list_module_items_mock):
-        list_module_ret = dumps([{"id": 0}])
-        list_modules_mock1.return_value.text = list_module_ret
-        list_modules_mock2.return_value.text = list_module_ret
-        list_module_items_mock.return_value.text = dumps([])
+    @patch(CONTROLLERS_LIST_ITEMS, return_value=APIReturn([]))
+    @patch(VIEWS_LIST_MODULES, return_value=APIReturn([{"id": 0}]))
+    @patch(CONTROLLERS_LIST_MODULES, return_value=APIReturn([{"id": 0}]))
+    def test_index_with_module(self, _mock1, _mock2, _mock3):
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "control_panel.html")
+    
+    @patch(CONTROLLERS_LIST_ITEMS, return_value=APIReturn([]))
+    @patch(CONTROLLERS_LIST_MODULES, return_value=APIReturn([]))
+    def test_get_uninstalled_treatments(self, _mock1, _mock2):
+        treatments = get_uninstalled_treatments(self.request)
+        self.assertEqual(len(treatments), 0)
+    
+    @patch(CONTROLLERS_LIST_ITEMS, return_value=APIReturn([]))
+    @patch(CONTROLLERS_LIST_MODULES, return_value=APIReturn([{"id": 0}]))
+    def test_get_uninstalled_treatments_with_item(self, _mock1, _mock2):
+        Treatment.objects.create(name="treatment1")
+        treatments = get_uninstalled_treatments(self.request)
+        self.assertEqual(len(treatments), 1)
+    
+    @patch(CONTROLLERS_LIST_MODULES, return_value=APIReturn([{"id": 0}]))
+    def test_get_uninstalled_treatments_against_api(self, _mock1):
+        treatment = Treatment.objects.create(name="treatment1")
+        mock_item = {"type": "ExternalTool",
+                     "external_url": treatment_url(self.request, treatment.id)}
+        with patch(CONTROLLERS_LIST_ITEMS, return_value=APIReturn([mock_item])):
+            treatments = get_uninstalled_treatments(self.request)
+            self.assertEqual(len(treatments), 0)
