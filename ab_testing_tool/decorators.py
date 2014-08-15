00843bbbb90c63ed594a91bc221b1fba0f3d6c02
Benjamin Zagorsky <iamzags@gmail.com>
2014-08-15 16:50:50 -0400

    Adds tests to page decorator, and changes logging to not log errors from page decorator in test mode

--- ab_testing_tool/decorators.py
+++ ab_testing_tool/decorators.py
@@ -6,6 +6,7 @@ from django.http.response import HttpResponse
 
 UNKNOWN_ERROR = "An unknown error occurred in the A/B testing tool"
 
+logger = logging.getLogger(__name__)
 
 def page(f):
     """ Decorator for page-view functions to handle errors; if an exception
@@ -27,8 +28,8 @@ def wrapped_page(f, args, kwargs):
         response = f(*args, **kwargs)
         return response
     except Exception as e:
-        logging.error(repr(e))
-        logging.error(traceback.format_exc())
+        logger.error(repr(e))
+        logger.error(traceback.format_exc())
         message = str(e)
         if not message:
             message = UNKNOWN_ERROR
@@ -37,26 +38,3 @@ def wrapped_page(f, args, kwargs):
 
 
 
-    def test_page_decorator_catches_error(self):
-        """ Tests that a page raising an exception renders the error template
-            with the correct message """
-        error_message = "This is a test error message"
-        @page
-        def test(request):
-            raise Exception(error_message)
-        
-        response = test(self.request)
-        self.assertTemplateUsed(response, "error.html")
-        self.assertContains(response, error_message)
-
-    def test_page_decorator_passthrough(self):
-        """ Tests that the page decorator doesn't modify the response of a 
-            page not raising an exception """
-        expected_response = HttpResponse("test")
-        @page
-        def test(request):
-            return expected_response
-        
-        response = test(self.request)
-        self.assertTemplateNotUsed(response, "error.html")
-        self.assertEqual(expected_response, response)
