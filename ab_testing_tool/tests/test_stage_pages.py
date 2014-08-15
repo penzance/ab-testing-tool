3df4634130c281a0f0a6500dd9834596df6ab924
kfantastic <kyangfan@gmail.com>
2014-08-15 15:38:46 -0400

    removes unncessary imports

--- ab_testing_tool/tests/test_stage_pages.py
+++ ab_testing_tool/tests/test_stage_pages.py
@@ -1,93 +1,16 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.test import TestCase
-from django.utils.importlib import import_module
-from json import dumps
-from mock import patch, MagicMock
+from mock import patch
 
-from ab_testing_tool.pages.main_pages import ADMINS, STAGE_URL_TAG
+from ab_testing_tool.pages.main_pages import STAGE_URL_TAG
 from ab_testing_tool.controllers import (get_uninstalled_stages,
-    stage_url, get_full_host)
+    stage_url)
 from ab_testing_tool.models import Stage, StageUrl
-from ab_testing_tool.canvas import InvalidResponseError, parse_response
-
-LIST_MODULES = "canvas_sdk.methods.modules.list_modules"
-LIST_ITEMS = "canvas_sdk.methods.modules.list_module_items"
-
-TEST_COURSE_ID = "12345"
-
-class SessionTestCase(TestCase):
-    def setUp(self):
-        # http://code.djangoproject.com/ticket/10899
-        settings.SESSION_ENGINE = "django.contrib.sessions.backends.file"
-        engine = import_module(settings.SESSION_ENGINE)
-        store = engine.SessionStore()
-        store.save()
-        self.session = store
-        self.client.cookies[settings.SESSION_COOKIE_NAME] = store.session_key
-        
-        settings.LOGGING = {}
-        
-        lti_launch = {}
-        lti_launch["roles"] = ADMINS
-        lti_launch["custom_canvas_course_id"] = TEST_COURSE_ID
-        lti_launch["lis_course_offering_sourcedid"] = "92345"
-        lti_launch["custom_canvas_api_domain"] = "example.com"
-        lti_launch["launch_presentation_return_url"] = "example.com"
-        session = self.client.session
-        session["LTI_LAUNCH"] = lti_launch
-        session.save()
-        
-        self.request = MagicMock()
-        self.request.session = session
-        self.request.get_host = MagicMock(return_value="example.com")
-        
-        # Patches api functions for all tests; can be overridden by re-patching
-        # the particular api call for a particular test
-        patchers = [
-            patch(LIST_MODULES, return_value=APIReturn([])),
-            patch(LIST_ITEMS, return_value=APIReturn([])),
-        ]
-        for patcher in patchers:
-            patcher.start()
-            self.addCleanup(patcher.stop)
-
-    def set_roles(self, roles):
-        session = self.client.session
-        session["LTI_LAUNCH"]["roles"] = roles
-        session.save()
+from ab_testing_tool.tests.test_main_pages import (SessionTestCase,
+    TEST_COURSE_ID, APIReturn, LIST_MODULES, LIST_ITEMS)
 
 
-class APIReturn(object):
-    """Spoofs returned response from Canvas SDK. Has response.ok property and JSON contents"""
-    def __init__(self, obj, ok=True):
-        self.text = dumps(obj)
-        self.ok = ok
-
-
-class test_views(SessionTestCase):
-    def test_index(self):
-        """Tests control_panel template rendered with nothing returned from Canvas"""
-        response = self.client.get(reverse("index"), follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "control_panel.html")
-
-    @patch(LIST_MODULES, return_value=APIReturn([{"id": 0}]))
-    def test_index_with_module_and_item(self, _mock1):
-        """Tests control_panel template rendered with items returned from Canvas"""
-        mock_item = {"type": "ExternalTool",
-                     "external_url": stage_url(self.request, 0)}
-        api_return = APIReturn([mock_item])
-        with patch(LIST_ITEMS, return_value=api_return):
-            response = self.client.get(reverse("index"), follow=True)
-            self.assertEqual(response.status_code, 200)
-            self.assertTemplateUsed(response, "control_panel.html")
-
-    def test_unauthenticated_index(self):
-        """Tests control_panel template not rendered when unauthorized"""
-        self.set_roles([])
-        response = self.client.get(reverse("index"), follow=True)
-        self.assertTemplateNotUsed(response, "control_panel.html")
+class test_stage_pages(SessionTestCase):
 
     def test_get_uninstalled_stages(self):
         """Tests method get_uninstalled_stages runs and returns no stages when database empty"""
@@ -112,26 +35,6 @@ class test_views(SessionTestCase):
         with patch(LIST_ITEMS, return_value=APIReturn([mock_item])):
             stages = get_uninstalled_stages(self.request)
             self.assertEqual(len(stages), 0)
-
-    def test_get_full_host(self):
-        """ Tests that appropriate prefix of http/https is used based on whether SSL is used"""
-        self.request.is_secure.return_value = False
-        self.assertIn("http://", get_full_host(self.request))
-        self.request.is_secure.return_value = True
-        self.assertIn("https://", get_full_host(self.request))
-
-    def test_parse_response_error(self):
-        """ Tests that a not OK API response raises an InvalidResponseError"""
-        response = APIReturn([])
-        response.ok = False
-        self.assertRaises(InvalidResponseError, parse_response, response)
-
-    def test_parse_response(self):
-        """ Tests that an OK API response is correctly returned"""
-        json_obj = [{"id": 0}]
-        response = APIReturn(json_obj)
-        response.ok = True
-        self.assertEquals(parse_response(response), json_obj)
 
     def test_submit_create_stage(self):
         #TODO: change this test to several tests that Stage and StageUrls in DB,
@@ -194,10 +97,3 @@ class test_views(SessionTestCase):
                 "id": stage.id}
         self.assertRaises(Exception, self.client.post,
                           reverse("submit_edit_stage"), data, follow=True)
-
-    def test_tool_config(self):
-        """ Tests that that tool_config page returns XML content"""
-        response = self.client.get(reverse("tool_config"))
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response._headers["content-type"],
-                        ('Content-Type', 'text/xml'))
