1606f002963e5e63e842923287141061ce35c4ce
Benjamin Zagorsky <iamzags@gmail.com>
2014-08-25 17:26:59 -0400

    adds missing files and updates

---
+++ ab_testing_tool/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for ab_testing_tool project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ab_testing_tool.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
