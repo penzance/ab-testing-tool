a9da7402e3eb9534f75dbbd34c9294882c3ccc3c
Benjamin Zagorsky <iamzags@gmail.com>
2014-09-09 16:44:07 -0400

    has middleware only display user defined exceptions and allows raising of custom status code

--- ab_testing_tool_app/exceptions.py
+++ ab_testing_tool_app/exceptions.py
@@ -1,26 +1,24 @@
-class InvalidResponseError(Exception):
-    """ TODO: Move this to canvas_sdk_python """
-    pass
+from error_middleware.middleware import UserDisplayError
 
-NO_SDK_RESPONSE = InvalidResponseError("Canvas SKD returned an error")
+NO_SDK_RESPONSE = UserDisplayError("Canvas SKD returned an error")
 
-MISSING_LTI_LAUNCH = Exception("LTI_LAUNCH not in session")
-MISSING_LTI_PARAM = Exception("Missing LTI parameter in session")
+MISSING_LTI_LAUNCH = UserDisplayError("LTI_LAUNCH not in session")
+MISSING_LTI_PARAM = UserDisplayError("Missing LTI parameter in session")
 
-BAD_STAGE_ID = Exception("Invalid stage id")
+BAD_STAGE_ID = UserDisplayError("Invalid stage id")
 
-MISSING_STAGE = Exception("No stage found matching database query")
-MISSING_TRACK = Exception("No track found matching database query")
+MISSING_STAGE = UserDisplayError("No stage found matching database query")
+MISSING_TRACK = UserDisplayError("No track found matching database query")
 
-DELETING_INSTALLED_STAGE = Exception("Deleting an installed Stage is not allowed")
+DELETING_INSTALLED_STAGE = UserDisplayError("Deleting an installed Stage is not allowed")
 
-UNAUTHORIZED_ACCESS = Exception("You do not have access to this course.")
+UNAUTHORIZED_ACCESS = UserDisplayError("You do not have access to this course.")
 
-MISSING_RETURN_TYPES_PARAM = Exception("Invalid ext_content_return_types")
+MISSING_RETURN_TYPES_PARAM = UserDisplayError("Invalid ext_content_return_types")
 
-MISSING_RETURN_URL = Exception("No ext_content_return_url")
+MISSING_RETURN_URL = UserDisplayError("No ext_content_return_url")
 
-COURSE_TRACKS_ALREADY_FINALIZED = Exception("You can't change tracks after their finalized")
-COURSE_TRACKS_NOT_FINALIZED = Exception("Course tracks are not finalized")
-NO_URL_FOR_TRACK = Exception("No course content. Ask your course instructors to put something here.")
-NO_TRACKS_FOR_COURSE = Exception("No tracks have been configured for this course.")
+COURSE_TRACKS_ALREADY_FINALIZED = UserDisplayError("You can't change tracks after their finalized")
+COURSE_TRACKS_NOT_FINALIZED = UserDisplayError("Course tracks are not finalized")
+NO_URL_FOR_TRACK = UserDisplayError("No course content. Ask your course instructors to put something here.")
+NO_TRACKS_FOR_COURSE = UserDisplayError("No tracks have been configured for this course.")
--- error_middleware/middleware.py
+++ error_middleware/middleware.py
@@ -8,9 +8,23 @@ logger = logging.getLogger(__name__)
 
 UNKNOWN_ERROR_STRING = "An unknown error occurred in the A/B testing tool"
 
+DEFAULT_ERROR_STATUS = 400
+
+
+class UserDisplayError(Exception):
+    status_code = DEFAULT_ERROR_STATUS
+    
+    def __init__(self, message="", status_code=DEFAULT_ERROR_STATUS):
+        self.status_code = status_code
+        super(UserDisplayError, self).__init__(message)
+
+
 class ErrorMiddleware(object):
     
     def process_exception(self, request, exception):
+        if not isinstance(exception, UserDisplayError):
+            return
+        status = exception.status_code
         logger.error(repr(exception))
         logger.error(traceback.format_exc())
         message = str(exception)
@@ -20,5 +34,5 @@ class ErrorMiddleware(object):
             template = loader.render_to_string("error.html", {"message": message})
         except TemplateDoesNotExist:
             logger.warn("No template 'error.html' found")
-            return HttpResponse("Error: %s" % message, status=400)
-        return HttpResponse(template, status=400)
+            return HttpResponse("Error: %s" % message, status=status)
+        return HttpResponse(template, status=status)
