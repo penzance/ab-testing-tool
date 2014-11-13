202eff7790f17df9f10ac0bef1c879bc3fbf05b0
kfantastic <kyangfan@gmail.com>
2014-11-13 18:45:43 -0500

    Revert "Reverts test files to `develop` branch; changes moved to branch tests/small... to be separately pull requested"
    This reverts commit 72d21ea1e37f8d598b65d9aa099fab20608b24be.

--- ab_tool/tests/test_canvas.py
+++ ab_tool/tests/test_canvas.py
@@ -1,13 +1,19 @@
 from ab_tool.tests.common import (SessionTestCase, APIReturn,
-    LIST_MODULES, LIST_ITEMS, TEST_COURSE_ID)
+    LIST_MODULES, LIST_ITEMS, TEST_COURSE_ID, TEST_OTHER_COURSE_ID)
 from ab_tool.canvas import (get_lti_param, list_modules,
-    list_module_items, get_canvas_request_context)
+    list_module_items, get_canvas_request_context, CanvasModules)
 from ab_tool.exceptions import (MISSING_LTI_LAUNCH, MISSING_LTI_PARAM,
     NO_SDK_RESPONSE)
 from mock import patch
 from requests.exceptions import RequestException
+from ab_tool.controllers import intervention_point_url
 
 class TestCanvas(SessionTestCase):
+    def get_canvas_modules(self, list_modules_return=[], list_items_return=[]):
+        with patch(LIST_MODULES, return_value=APIReturn(list_modules_return)):
+            with patch(LIST_ITEMS, return_value=APIReturn(list_items_return)):
+                return CanvasModules(self.request)
+    
     def test_get_lti_param_success(self):
         """ Tests that get_lti_param returns the correct value when it is present """
         param_val = "test_param_val"
@@ -54,3 +60,71 @@ class TestCanvas(SessionTestCase):
         request_context = get_canvas_request_context(self.request)
         self.assertRaisesSpecific(NO_SDK_RESPONSE, list_modules,
                                   request_context, TEST_COURSE_ID)
+    
+    def test_get_uninstalled_intervention_points(self):
+        """ Tests method get_uninstalled_intervention_points runs and returns no intervention_points when
+            database empty """
+        intervention_points = self.get_canvas_modules().get_uninstalled_intervention_points()
+        self.assertEqual(len(intervention_points), 0)
+    
+    def test_get_uninstalled_intervention_points_with_item(self):
+        """ Tests method get_uninstalled_intervention_points returns one when database has
+            one item and api returns nothing """
+        self.create_test_intervention_point()
+        canvas_modules = self.get_canvas_modules(list_modules_return=[{"id": 0}])
+        intervention_points = canvas_modules.get_uninstalled_intervention_points()
+        self.assertEqual(len(intervention_points), 1)
+    
+    def test_get_uninstalled_intervention_points_against_courses(self):
+        """ Tests method get_uninstalled_intervention_points returns one when database has
+            two items but only one matches the course and api returns nothing """
+        intervention_point = self.create_test_intervention_point(name="ip1")
+        self.create_test_intervention_point(name="ip2", course_id=TEST_OTHER_COURSE_ID)
+        canvas_modules = self.get_canvas_modules(list_modules_return=[{"id": 0}])
+        intervention_points = canvas_modules.get_uninstalled_intervention_points()
+        self.assertEqual(len(intervention_points), 1)
+        self.assertSameIds([intervention_point], intervention_points)
+    
+    def test_get_uninstalled_intervention_points_with_all_installed(self):
+        """ Tests method get_uninstalled_intervention_points returns zero when intervention_point in
+            database is also returned by the API, which means it is installed """
+        intervention_point = self.create_test_intervention_point()
+        mock_item = {"type": "ExternalTool",
+                     "external_url": intervention_point_url(self.request, intervention_point.id)}
+        canvas_modules = self.get_canvas_modules(list_modules_return=[{"id": 0}], list_items_return=[mock_item])
+        intervention_points = canvas_modules.get_uninstalled_intervention_points()
+        self.assertEqual(len(intervention_points), 0)
+    
+    def test_get_uninstalled_intervention_points_with_some_installed(self):
+        """ Tests method get_uninstalled_intervention_points returns one when there are two
+            intervention_points in the database, one of which is also returned by the API,
+            which means it is installed """
+        intervention_point = self.create_test_intervention_point(name="ip1")
+        self.create_test_intervention_point(name="ip2")
+        mock_item = {"type": "ExternalTool",
+                     "external_url": intervention_point_url(self.request, intervention_point.id)}
+        canvas_modules = self.get_canvas_modules(list_modules_return=[{"id": 0}], list_items_return=[mock_item])
+        intervention_points = canvas_modules.get_uninstalled_intervention_points()
+        self.assertEqual(len(intervention_points), 1)
+    
+    def test_all_intervention_point_urls_empty(self):
+        """ Tests that all_intervention_point_urls returns empty when there are no intervention_points """
+        urls = self.get_canvas_modules()._all_intervention_point_urls()
+        self.assertEqual(len(urls), 0)
+    
+    def test_all_intervention_point_urls_one_element(self):
+        """ Tests that all_intervention_point_urls returns the url for one intervention_point when
+            that is in the database """
+        intervention_point = self.create_test_intervention_point()
+        urls = self.get_canvas_modules()._all_intervention_point_urls()
+        self.assertEqual(len(urls), 1)
+        self.assertEqual({intervention_point_url(self.request, intervention_point.id): intervention_point}, urls)
+    
+    def test_all_intervention_point_urls_multiple_courses(self):
+        """ Tests that all_intervention_point_urls only returns the url for the intervention_point
+            in the database that matches the course_id """
+        intervention_point = self.create_test_intervention_point(name="ip1")
+        self.create_test_intervention_point(name="ip2", course_id=TEST_OTHER_COURSE_ID)
+        urls = self.get_canvas_modules()._all_intervention_point_urls()
+        self.assertEqual(len(urls), 1)
+        self.assertEqual({intervention_point_url(self.request, intervention_point.id): intervention_point}, urls)
