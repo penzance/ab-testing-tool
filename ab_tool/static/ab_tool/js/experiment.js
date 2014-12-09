d9719fa7409ac2054f2e6c91199e0149a861d14e
Benjamin Zagorsky <iamzags@gmail.com>
2014-12-09 17:24:00 -0500

    adds comments and reduces some duplication

--- ab_tool/static/ab_tool/js/experiment.js
+++ ab_tool/static/ab_tool/js/experiment.js
@@ -9,24 +9,40 @@ angular.module('ABToolExperiment', []).controller(
     $scope.newTrackWeighting = null;
     
     $scope.addTrack = function() {
+        /**
+         * Add a new track based on the inputs in the new track form
+         * (the fields in the new track form are bound to the variables
+         * $scope.newTrackName and $scope.newTrackWeighting)
+         */
+        // Do nothing if newTrackName is empty
         if (!$scope.newTrackName) {
             return;
         }
+        // Ignore newTrackWeighting if we are in uniformRandom mode
         if ($scope.experiment.uniformRandom) {
             $scope.newTrackWeighting = null;
         }
         
         $scope.experiment.tracks.push({id: null, name: $scope.newTrackName,
             weighting: $scope.newTrackWeighting});
+        // Clear the new track form
         $scope.newTrackName = null;
         $scope.newTrackWeighting = null;
     };
     
     $scope.uniformPercent = function() {
+        /**
+         * Returns the percentage that should be displayed as weighting for
+         * each track when uniformRandom is true.
+         * Ex. if there are three tracks, this will return 33.
+         */
         return Math.round(100 / $scope.experiment.tracks.length);
     }
     
     $scope.submit = function() {
+        /**
+         * Submits form contents to the backend and redirects to window.parentPage
+         */
         // Payload has to be encoded using JQuery's $.param to submit properly
         var payload = $.param({"experiment": JSON.stringify($scope.experiment)});
         $http.post($window.submitURL, payload).
@@ -39,6 +55,13 @@ angular.module('ABToolExperiment', []).controller(
     }
     
     $scope.difference = function() {
+        /**
+         * Returns true iff the experiment currently in the form differs from
+         * the one the form started with, so that a confirmation can be
+         * selectively displayed on cancelChanges.  Note that deleted tracks
+         * are not included in the difference because track deletion is
+         * confirmed separately.
+         */
         var orig = $window.initialExperiment;
         var curr = $scope.experiment;
         if (orig.name != curr.name) { return true; }
@@ -65,10 +88,14 @@ angular.module('ABToolExperiment', []).controller(
     }
     
     $scope.cancel = function() {
+        /**
+         * Navigates to parent page without saving changes on backend;
+         * confirms cancelation if changes have been made (as determined by
+         * the difference function).
+         */
+        var message = "You have unsaved changes. Are you sure you want to cancel?";
         var confirmCancel = $scope.difference();
-        if (!confirmCancel) {
-            $window.location = $window.parentPage;
-        } else if (confirm("You have unsaved changes. Are you sure you want to cancel?") == true) {
+        if (!confirmCancel || confirm(message)) {
             $window.location = $window.parentPage;
         }
     };
