45399692bfbc9201a90e34ea86657138313afc5b
Benjamin Zagorsky <iamzags@gmail.com>
2014-12-30 19:25:04 -0500

    remvoes changes from cvs upload branch to create a branch with just the spreadsheet refactor

--- ab_tool/spreadsheets.py
+++ ab_tool/spreadsheets.py
@@ -1,11 +1,5 @@
-import xlsxwriter
-import StringIO
-import xlrd
-from django.http.response import HttpResponse
-
 from ab_tool.models import (ExperimentStudent, InterventionPointInteraction)
 from ab_tool.controllers import streamed_csv_response
-from ab_tool.canvas import get_unsorted_students
 
 
 def get_student_list_csv(experiment, file_title):
@@ -28,41 +22,3 @@ def get_intervention_point_interactions_csv(experiment, file_title):
                    i.intervention_point.name, i.url, i.created_on]
     return streamed_csv_response(row_generator(), file_title)
 
-
-def get_track_selection_xlsx(request, experiment, file_title="test.xlsx"):
-    output = StringIO.StringIO()
-    workbook = xlsxwriter.Workbook(output, {"in_memory": True})
-    worksheet = workbook.add_worksheet()
-    worksheet.set_column(0, 3, 20)
-    headers = ["Student ID", "LIS Person Sourcedid", "Experiment", "Assigned Track"]
-    worksheet.write_row(0, 0, headers)
-    students = get_unsorted_students(request, experiment)
-    # Row offsets of +1 below are to account for header
-    for i, student in enumerate(students):
-        row = [student["student_id"], student["lis_person_sourcedid"], experiment.name, ""]
-        worksheet.write_row(i + 1, 0, row)
-    # Add drop-down with track names as the only valid options for the missing column
-    track_names = [t.name for t in experiment.tracks.all()]
-    worksheet.data_validation(1, 3, len(students) + 1, 3,
-                              {'validate': 'list', 'source': track_names,})
-    workbook.close()
-    output.seek(0)
-    content_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    response = HttpResponse(output.read(), content_type=content_type)
-    response['Content-Disposition'] = ("attachment; filename=%s" % file_title)
-    return response
-
-
-def get_track_selection_csv(request, experiment, file_title="test.xlsx"):
-    def row_generator():
-        yield ["Student ID", "LIS Person Sourcedid", "Experiment", "Assigned Track"]
-        # Write data to CSV file
-        for s in get_unsorted_students(request, experiment):
-            yield [s["student_id"], s["lis_person_sourcedid"], experiment.name, ""]
-    return streamed_csv_response(row_generator(), file_title)
-
-
-def parse_track_selection_xlsx(input_excel):
-    workbook = xlrd.open_workbook(file_contents=input_excel.read())
-    worksheet = workbook.sheet_by_index(0)
-    # http://www.youlikeprogramming.com/2012/03/examples-reading-excel-xls-documents-using-pythons-xlrd/
