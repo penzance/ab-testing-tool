ced585582d559b6c20aa69951c439296bef76513
Benjamin Zagorsky <iamzags@gmail.com>
2015-01-22 14:28:58 -0500

    changes organization of the test file

--- spec/javascripts/experiment_spec.js
+++ spec/javascripts/experiment_spec.js
@@ -1,4 +1,4 @@
-describe("Tests for experiment", function() {
+describe("Tests for experimentController", function() {
     var $scope, $controller, controller, experiment;
     
     beforeEach(module('ABToolExperiment'));
@@ -8,24 +8,39 @@ describe("Tests for experiment", function() {
         $controller = _$controller_;
     }));
     
-    describe('experimentController', function() {
-        beforeEach(function() {
-            experiment = {
-                    "name": "", "notes": "", "uniformRandom": true,
-                    "tracks": [{"id": null, "name": "A", "newName": "A"},
-                               {"id": null, "name": "B", "newName": "B"}]};
-            $scope = {};
-            controller = $controller('experimentController', { $scope: $scope });
-            $scope.experiment = experiment;
+    beforeEach(function() {
+        experiment = {
+                "name": "", "notes": "", "uniformRandom": true,
+                "tracks": [{"id": null, "name": "A", "newName": "A"},
+                           {"id": null, "name": "B", "newName": "B"}]};
+        $scope = {};
+        controller = $controller('experimentController', { $scope: $scope });
+        $scope.experiment = experiment;
+    });
+    
+    describe("Tests for uniformPercent", function() {
+        it("uniformPercent is 50 for two tracks", function() {
+            expect($scope.uniformPercent()).toBe(50);
         });
         
+        it("uniformPercent is 33 for three tracks", function() {
+            $scope.experiment.tracks.push({});
+            expect($scope.uniformPercent()).toBe(33);
+        });
+    });
+    
+    
+    describe("Miscelaneous tests", function() {
         it("name is empty to start", function() {
             expect($scope.experiment.name).toBe("");
         });
         
-        it("uniformPercent is 50 for two tracks", function() {
-            expect($scope.uniformPercent()).toBe(50);
-        });
+//        it("test submit", function() {
+//            $scope.submit();
+//            expect($scope.experiment.name).toBe("");
+//        });
     });
+    
+
 });
 
