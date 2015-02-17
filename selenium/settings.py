a016d95f99da1f3517dd1b6ca4ddef5a0981ece4
Marlee Chong <marlee_chong@harvard.edu>
2015-02-17 18:59:51 -0500

    Running the Selenium tests off of QA instead of test

--- selenium/settings.py
+++ selenium/settings.py
@@ -9,7 +9,8 @@ USERS = {
 canvas_environment = "canvas.icommons.harvard.edu"
 canvas_base_url = "https://" + canvas_environment + "/courses/6284/"
 
-tool_server = "https://test.tlt.harvard.edu/ab-testing"
+tool_server = "https://qa.tlt.harvard.edu/ab-testing"
+#tool_server = "https://test.tlt.harvard.edu/ab-testing"
 #tool_server = "localhost:8000/ab-testing"
 
 import time
