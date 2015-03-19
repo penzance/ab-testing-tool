bc624dee5c18062dd356e0f9b1f3962f939a5852
Yates, Elliott <elliott_yates@harvard.edu>
2015-03-19 12:04:55 -0400

    IP modals: notes field now resets in edit IP modal, preview link hidden by default on create IP modal, changes some JS methods to camel case

--- ab_tool/static/ab_tool/js/experiments_dashboard.js
+++ ab_tool/static/ab_tool/js/experiments_dashboard.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
     // *** IP MODALS *************************************************************************************************
 
-    function validate_ip_name_input_element($name) {
+    function validateIPNameInputElement($name) {
         $name.toggleClass('has-error empty', !($name.val().trim()));
     }
 
-    function validate_url_input_element($url) {
+    function validateUrlInputElement($url) {
         // todo: consider using custom HTML5 validation with .willValidate() or .is(':valid') instead
         var urlValue = $url.val().trim();
         $url.toggleClass('empty', !urlValue);
-        if ( is_valid_url(urlValue) ){
+        if ( isValidUrl(urlValue) ){
             $url.removeClass('has-error');
             $url.siblings('.preview-link').attr('href', urlValue).show();
         } else {
@@ -19,42 +19,45 @@ $(document).ready(function(){
         }
     }
 
-    function is_valid_url(url) {
+    function isValidUrl(url) {
         //if URL input element is used, this should match the pattern attribute or the custom validation
         return /^(http(s)?:\/\/)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test(url);
     }
 
-    function enable_submit_if_no_errors($form) {
+    function enableSubmitIfNoErrors($form) {
         var formIsSubmittedPendingResponse = $form.find('.modal-submit').hasClass('submitting');
         var validationErrorsFound = $form.find('.has-error, .empty').length != 0;
         $form.find('.modal-submit').toggleClass('disabled', formIsSubmittedPendingResponse || validationErrorsFound);
     }
 
-    function reset_IP_create_form($form) {
+    function resetIPCreateForm($form) {
+        // reset values for non-selectmenu fields
         $form.find('.form-control').val('');
+        // hide preview links for blank URL fields
+        $form.find('.preview-link').hide();
         // .empty ensures that form cannot be submitted even though .has-error is not showing user validation feedback
         $form.find('.intervention-url, .intervention-name').removeClass('has-error').addClass('empty');
         // reset each selectmenu to the first item in the menu
         $form.find('select').each(function () {
             $($(this).siblings('ul').find('a')[0]).trigger('select');
         });
-        enable_submit_if_no_errors($form);
+        enableSubmitIfNoErrors($form);
     }
 
-    function reset_IP_edit_form($form) {
-        $form.find('.intervention-url, .intervention-name').each(function () {
+    function resetIPEditForm($form) {
+        $form.find('.intervention-url, .intervention-name, .intervention-notes').each(function () {
             $(this).val($(this).data("initial-value"));
         });
         $form.find('.intervention-url').each(function(){
-            validate_url_input_element($(this));
+            validateUrlInputElement($(this));
         });
         $form.find('.intervention-name').each(function(){
-            validate_ip_name_input_element($(this));
+            validateIPNameInputElement($(this));
         });
         $form.find('select').each(function () {
             $($(this).siblings('ul').find('a')[parseInt($(this).data("initial-index"))]).trigger('select');
         });
-        enable_submit_if_no_errors($form);
+        enableSubmitIfNoErrors($form);
     }
 
     // Initialize elements ****************************************************************************************
@@ -69,11 +72,11 @@ $(document).ready(function(){
     });
 
     $('[data-modal="intervention-new"]').each(function() {
-        reset_IP_create_form($(this).find('.modal-form'));
+        resetIPCreateForm($(this).find('.modal-form'));
     });
 
     $('[data-modal="intervention-edit"]').each(function() {
-        reset_IP_edit_form($(this).find('.modal-form'));
+        resetIPEditForm($(this).find('.modal-form'));
     });
 
     // Set up event handlers ****************************************************************************************
@@ -93,24 +96,24 @@ $(document).ready(function(){
     });
 
     $('.intervention-url').on('input', function(){
-        validate_url_input_element($(this));
-        enable_submit_if_no_errors($(this).closest('.modal-form'));
+        validateUrlInputElement($(this));
+        enableSubmitIfNoErrors($(this).closest('.modal-form'));
     });
 
     $('.intervention-name').on('input', function () {
-        validate_ip_name_input_element($(this));
-        enable_submit_if_no_errors($(this).closest('.modal-form'));
+        validateIPNameInputElement($(this));
+        enableSubmitIfNoErrors($(this).closest('.modal-form'));
     });
 
     // Reset the modal intervention point forms when they are dismissed (cancel, esc, submit, etc)
     // Note: show.bs.modal is triggered by all selectmenu clicks, so we reset the forms on hidden.bs.modal,
     //       as otherwise we have to check event target in show.bs.modal to avoid resetting on every click
     $('[data-modal="intervention-new"]').on('hidden.bs.modal', function() {
-        reset_IP_create_form($(this).find('.modal-form'));
+        resetIPCreateForm($(this).find('.modal-form'));
     });
 
     $('[data-modal="intervention-edit"]').on('hidden.bs.modal', function() {
-        reset_IP_edit_form($(this).find('.modal-form'));
+        resetIPEditForm($(this).find('.modal-form'));
     });
 
     // *** DASHBOARD *******************************************************************************************
