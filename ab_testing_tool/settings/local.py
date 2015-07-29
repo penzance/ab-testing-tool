e39000456a6a0db585b367048faa0f56a2345c94
hktest <hiukei_chow@harvard.edu>
2015-07-29 12:11:10 -0400

    Added the Selenium Config to the base file in order to run selenium tests

--- ab_testing_tool/settings/local.py
+++ ab_testing_tool/settings/local.py
@@ -1,3 +1,10 @@
 from .base import *
 
 INSTALLED_APPS += ('debug_toolbar', 'sslserver')
+
+SELENIUM_CONFIG = {
+   'selenium_username': ENV_SETTINGS.get('selenium_user'),
+   'selenium_password': ENV_SETTINGS.get('selenium_password'),
+   'selenium_grid_url': ENV_SETTINGS.get('selenium_grid_url'),
+   'base_url': 'https://canvas.icommons.harvard.edu',
+}
