0a029942d00283ba87be36147f5ead7e08f6115c
hktest <hiukei_chow@harvard.edu>
2015-09-22 13:32:03 -0400

    - Modified the PIN login page object for the ab_testing_tool_project, due to new Harvard Key Pin Login screen.

--- selenium_tests/pin/page_objects/pin_page.py
+++ selenium_tests/pin/page_objects/pin_page.py
@@ -1,6 +1,4 @@
-"""
-This file models some of the page objects on the Manage People Search Page.  (find_user.html)
-"""
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium_tests.pin.page_objects.base_page_object import PinBasePageObject
@@ -8,10 +6,10 @@ import abc
 
 class PinPageLocators(object):
     # List of WebElements found on Pin Page (Locators)
-    LOGIN_TYPE_XID_RADIO_BUTTON = (By.CSS_SELECTOR, "input[value=\"XID\"]")
     USERNAME = (By.ID, "username")
     PASSWORD = (By.ID, "password")
-    SUBMIT_BUTTON = (By.CSS_SELECTOR, "input[name=\"_eventId_submit\"]")
+    SUBMIT_BUTTON = (By.ID,"submitLogin")
+    LoginType = (By.XPATH, "//div[@id='XID']")
 
 
 class PinLoginPageObject(PinBasePageObject):
@@ -23,10 +21,16 @@ class PinLoginPageObject(PinBasePageObject):
     def is_loaded(self):
         """ determine if the page loaded by looking for a specific element on the page """
         try:
-            self.find_element(*PinPageLocators.LOGIN_TYPE_XID_RADIO_BUTTON)
+            self.find_element(*PinPageLocators.USERNAME)
         except NoSuchElementException:
             return False
         return True
+
+
+    def set_login_type_xid(self):
+        """ set the login type to XID """
+        comp_auth_source_type_element = self.find_element(*PinPageLocators.LoginType)
+        comp_auth_source_type_element.click()
 
     def set_username(self, username):
         """ set the username """
@@ -40,11 +44,6 @@ class PinLoginPageObject(PinBasePageObject):
         password_element.clear()
         password_element.send_keys(password)
 
-    def set_login_type_xid(self):
-        """ set the compositeAuthenticationSourceType3 option """
-        comp_auth_source_type_element = self.find_element(*PinPageLocators.LOGIN_TYPE_XID_RADIO_BUTTON)
-        comp_auth_source_type_element.click()
-
     def click_submit(self):
         """ click the submit button """
         submit_button = self.find_element(*PinPageLocators.SUBMIT_BUTTON)
@@ -57,7 +56,7 @@ class PinLoginPageObject(PinBasePageObject):
         """
         the abstract method can be overridden for individual projects to allow
         the login to return the appropriate page object for the test. If you do override
-        you will need to call supper to invoke the login
+        you will need to call super to invoke the login
          """
         print 'base class logging in user: %s' % username
         self.set_login_type_xid()
